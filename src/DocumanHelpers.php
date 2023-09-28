4ffe960fda0b1386c13d6af442dd18d32f0dffc7
tekkenking <mytansill@yahoo.com>
2023-09-28 11:51:45 +0100

    added more helpers

--- src/DocumanHelpers.php
+++ src/DocumanHelpers.php
@@ -1,8 +1,30 @@
 <?php
 
+use Tekkenking\Documan\Documan as DocumanAlias;
+use Tekkenking\Documan\DocumanCollections;
+
 if(! function_exists('documan')) {
     function documan(string $disk = '') {
         return app('documan', [$disk]);
+    }
+}
+
+if(! function_exists('documan_collections')) {
+    function documan_collections($documan) {
+       return new DocumanCollections($documan);
+    }
+}
+
+if (!function_exists('convertImageToBase64')) {
+
+    /**
+     * @param $imagePath
+     * @param $size
+     * @return string
+     */
+    function convertImageToBase64($imagePath, $size = 'original'): string
+    {
+        return base64_encode(file_get_contents($imagePath->localPath($size)));
     }
 }
 
