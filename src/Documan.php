d76dad25b07406f84622059d061ae3b8e7aef6f9
tekkenking <mytansill@yahoo.com>
2023-10-02 20:23:25 +0100

    fixed issues with size method call ignored

--- src/Documan.php
+++ src/Documan.php
@@ -178,54 +178,6 @@ class Documan
 
     }
 
-    public function sizes(array $sizes = []): Documan
-    {
-        if(!empty($sizes)) {
-            $workingSizes = [];
-
-            foreach ($sizes as $size => $value) {
-                if(is_int($size)) {
-                    //This mean it's unassociated array
-                    //Is this available amongst the default sizes
-                    if(!isset($this->defaultSizes[$value])) {
-                        dd("{$value} is not a valid size");
-                    }
-                    $workingSizes[$value] = $this->defaultSizes[$value];
-                } else {
-                    //Meaning it's an associated array that should have width and height
-
-                    //Let's make sure it's properly formed with width and height
-                    if(!is_array($value)) {
-                        dd("{$size} value must be properly formed array with width or height or both");
-                    } else {
-                        if(isset($this->defaultSizes[$size])) {
-                            //Meaning we want to overwrite config size at run time
-                            if(isset($value['width'])) {
-                                $this->defaultSizes[$size]['width'] = $value['width'];
-                            }
-
-                            if(isset($value['height'])) {
-                                $this->defaultSizes[$size]['height'] = $value['height'];
-                            }
-                        } else {
-                            //We are adding customer size type at run time
-                            if(!isset($value['width']) || !isset($value['height'])) {
-                                dd("{$size} value must be properly formed array with width and height");
-                            }
-
-                            $this->defaultSizes[$size]['width'] = $value['width'];
-                            $this->defaultSizes[$size]['height'] = $value['height'];
-                        }
-                    }
-                    $workingSizes[$size] = $this->defaultSizes[$size];
-                }
-            }
-
-            $this->chosenSizes = $workingSizes;
-        }
-        return $this;
-    }
-
 
 
     /**
